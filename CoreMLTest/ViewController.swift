3276ce6ee2f41b19a2e3e51d24b72fe3ced25bd9
Jelena Tasic <jelenatasicsd@gmail.com>
2021-10-24 10:02:42 +0200

    Choose image from photo library.

--- CoreMLTest/ViewController.swift
+++ CoreMLTest/ViewController.swift
@@ -6,14 +6,23 @@
 //
 
 import UIKit
+import CoreML
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
 
+    @IBOutlet weak var imageView: UIImageView!
+    let imagePicker = UIImagePickerController()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        imagePicker.delegate = self
+        imagePicker.allowsEditing = false
+        imagePicker.sourceType = .photoLibrary
     }
 
-
+    @IBAction func cameraTapped(_ sender: UIBarButtonItem) {
+        present(imagePicker, animated: true, completion: nil)
+    }
 }
 
